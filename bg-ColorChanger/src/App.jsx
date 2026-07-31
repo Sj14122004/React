5b5427805f2224f6ca83dc29ed526a45d30c31c5
Sj14122004 <joshishivam2004@gmail.com>
2026-07-31 10:45:44 +0530

    MINIProject

---
+++ bg-ColorChanger/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+function App() {
+  const[color, setColor] = useState("olive")
+  return (
+    <div className="w-full h-screen duration-200"
+    style = {{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button className="outline-none px-1 rounded-3xl text-white shadow-lg hover:scale-110 transition-transform duration-200"
+          style = {{backgroundColor: "red"}}
+          onClick= {() => setColor("red")}
+          >Red</button>
+          <button className="outline-none px-1 rounded-3xl text-white shadow-lg hover:scale-110 transition-transform duration-200"
+          style = {{backgroundColor: "Green"}}
+          onClick = {() => setColor("Green")}
+          >Green</button>
+          <button className="outline-none px-1 rounded-3xl text-white shadow-lg hover:scale-110 transition-transform duration-200"
+          style = {{backgroundColor: "black"}}
+          onClick={() => setColor("black")}
+          >Black</button>
+          <button className="outline-none px-1 rounded-3xl text-white shadow-lg hover:scale-110 transition-transform duration-200"
+          style = {{backgroundColor: "purple"}}
+          onClick={() => setColor("purple")}
+          >Purple</button>
+          <button className="outline-none px-1 rounded-3xl text-white shadow-lg hover:scale-110 transition-transform duration-200"
+          style = {{backgroundColor: "orange"}}
+          onClick={() => setColor("orange")}
+          >Orange</button>
+          <button className="outline-none px-1 rounded-3xl text-white shadow-lg hover:scale-110 transition-transform duration-200"
+          style = {{backgroundColor: "Pink"}}
+          onClick={() => setColor("pink")}
+          >Pink</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default App
